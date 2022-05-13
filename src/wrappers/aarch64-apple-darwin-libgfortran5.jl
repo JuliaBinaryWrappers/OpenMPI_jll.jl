c5c7f896233bb262d0dcac3b96f10c7ed5e9936f
jlbuild <juliabuildbot@gmail.com>
2022-05-13 22:49:31 +0000

    OpenMPI_jll build 4.1.3+0

--- src/wrappers/aarch64-apple-darwin-libgfortran5.jl
+++ src/wrappers/aarch64-apple-darwin-libgfortran5.jl
@@ -3,17 +3,12 @@ export generate_compile_time_mpi_constants, libload_time_mpi_constants, libmpi, 
 
 using CompilerSupportLibraries_jll
 JLLWrappers.@generate_wrapper_header("OpenMPI")
-JLLWrappers.@declare_executable_product(generate_compile_time_mpi_constants)
 JLLWrappers.@declare_library_product(libload_time_mpi_constants, "@rpath/libload_time_mpi_constants.dylib")
 JLLWrappers.@declare_library_product(libmpi, "@rpath/libmpi.40.dylib")
+JLLWrappers.@declare_executable_product(generate_compile_time_mpi_constants)
 JLLWrappers.@declare_executable_product(mpiexec)
 function __init__()
     JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll)
-    JLLWrappers.@init_executable_product(
-        generate_compile_time_mpi_constants,
-        "bin/generate_compile_time_mpi_constants",
-    )
-
     JLLWrappers.@init_library_product(
         libload_time_mpi_constants,
         "lib/libload_time_mpi_constants.dylib",
@@ -24,6 +19,11 @@ function __init__()
         libmpi,
         "lib/libmpi.40.dylib",
         RTLD_LAZY | RTLD_DEEPBIND,
+    )
+
+    JLLWrappers.@init_executable_product(
+        generate_compile_time_mpi_constants,
+        "bin/generate_compile_time_mpi_constants",
     )
 
     JLLWrappers.@init_executable_product(
--- src/wrappers/i686-linux-gnu-libgfortran3.jl
+++ src/wrappers/i686-linux-gnu-libgfortran3.jl
@@ -3,17 +3,12 @@ export generate_compile_time_mpi_constants, libload_time_mpi_constants, libmpi, 
 
 using CompilerSupportLibraries_jll
 JLLWrappers.@generate_wrapper_header("OpenMPI")
-JLLWrappers.@declare_executable_product(generate_compile_time_mpi_constants)
 JLLWrappers.@declare_library_product(libload_time_mpi_constants, "libload_time_mpi_constants.so")
 JLLWrappers.@declare_library_product(libmpi, "libmpi.so.40")
+JLLWrappers.@declare_executable_product(generate_compile_time_mpi_constants)
 JLLWrappers.@declare_executable_product(mpiexec)
 function __init__()
     JLLWrappers.@generate_init_header(CompilerSupportLibraries_jll)
-    JLLWrappers.@init_executable_product(
-        generate_compile_time_mpi_constants,
-        "bin/generate_compile_time_mpi_constants",
-    )
-
     JLLWrappers.@init_library_product(
         libload_time_mpi_constants,
         "lib/libload_time_mpi_constants.so",
@@ -24,6 +19,11 @@ function __init__()
         libmpi,
         "lib/libmpi.so",
         RTLD_LAZY | RTLD_DEEPBIND,
+    )
+
+    JLLWrappers.@init_executable_product(
+        generate_compile_time_mpi_constants,
+        "bin/generate_compile_time_mpi_constants",
     )
 
     JLLWrappers.@init_executable_product(
